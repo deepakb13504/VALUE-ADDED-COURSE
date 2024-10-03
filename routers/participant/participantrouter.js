9446da737d78222898214313ca8bcee905a71146
kannan <kannanp03022004@gmail.com>
2024-10-03 14:18:00 +0530

    put and delete router

--- routers/participant/participantrouter.js
+++ routers/participant/participantrouter.js
@@ -18,5 +18,31 @@ router.post("/post", async (req, res) => {
     } catch (error) {
       res.send("error");
     }
+  }),
+  router.put("/put/:id", async (req, res) => {
+    try {
+      const participant = await Participant.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true, runValidators: true }
+      );
+      if (!participant) {
+        return res.send("Participant is not found");
+      }
+      res.send(participant);
+    } catch (error) {
+      res.send("error");
+    }
+  }),
+  router.delete("/delete/:id", async (req, res) => {
+    try {
+      const participant = await Participant.findByIdAndDelete(req.params.id);
+      if (!participant) {
+        return res.send("Participant is not found");
+      }
+      res.send("deleted");
+    } catch (error) {
+      res.send(error);
+    }
   });
 export default router;
