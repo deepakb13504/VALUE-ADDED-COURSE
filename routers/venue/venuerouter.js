c8be6a65006f9e230501ee94ec22c70d51065913
kannan <kannanp03022004@gmail.com>
2024-10-03 15:01:40 +0530

    put and delete router

--- routers/venue/venuerouter.js
+++ routers/venue/venuerouter.js
@@ -18,5 +18,30 @@ router.post("/post", async (req, res) => {
     } catch (error) {
       res.send("error");
     }
+  }),
+  router.put("/put/:id", async (req, res) => {
+    try {
+      const venue = await Venue.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (!venue) {
+        return res.send("Venue is not found");
+      }
+      res.send(venue);
+    } catch (error) {
+      res.send("error");
+    }
   });
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const venue = await Venue.findByIdAndDelete(req.params.id);
+    if (!venue) {
+      return res.send("Venue is not found");
+    }
+    res.send("deleted");
+  } catch (error) {
+    res.send(error);
+  }
+});
 export default router;
